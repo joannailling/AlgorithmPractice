975fafda68a31a4149b10d1a83a82fd0f359cf01
Joanna Illing <illing.joanna@gmail.com>
2015-09-14 10:48:30 -0700

    Problem 1.2 to reverse C-string
    Write code to reverse a C-Style String  (C-String means that “abcd” is
    represented as five characters, including the null character )

--- src/CrackCodingInt/Chapter1.java
+++ src/CrackCodingInt/Chapter1.java
@@ -29,8 +29,35 @@ public class Chapter1 {
         }
     } 
     
+    public String onePointTwo(String inString) {
+        // Write code to reverse a C-Style String  (C-String means that “abcd” is 
+        // represented as five characters, including the null character )
+        char temp;
+        char[] chars = inString.toCharArray();
+        int lo = 0;
+        int hi = chars.length - 2;
+        while (lo < hi) {
+            temp = chars[lo];
+            chars[lo] = chars[hi];
+            chars[hi] = temp;
+            lo++;
+            hi--;
+        } 
+        return String.valueOf(chars);
+    }
+    
+    public void testOnePointTwo() {
+        scan  = new Scanner(System.in);
+        for (int i = 0; i < 5; i++ ){
+            System.out.print("Enter a String: ");
+            String s = onePointTwo(scan.nextLine());
+            System.out.println(s);
+        }
+    } 
+    
     public static void main(String[] args) {
         Chapter1 c1 = new Chapter1();
-        c1.testOnePointOne();
+        //c1.testOnePointOne();
+        c1.testOnePointTwo();
     }
 }
