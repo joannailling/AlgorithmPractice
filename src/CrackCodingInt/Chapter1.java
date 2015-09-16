dede8359e1e91d9cb6b1dcf69455d63a2fb702eb
Joanna Illing <illing.joanna@gmail.com>
2015-09-16 15:32:24 -0700

    1.3 and reworked test function
    Added code for 1.3 to remove duplicates from a string.
    Reworked test functions into one with case/switch so that user can enter
    problem number and number of repetitions.

--- src/CrackCodingInt/Chapter1.java
+++ src/CrackCodingInt/Chapter1.java
@@ -20,15 +20,6 @@ public class Chapter1 {
         return true;
     }
     
-    public void testOnePointOne() {
-        scan  = new Scanner(System.in);
-        for (int i = 0; i < 5; i++ ){
-            System.out.print("Enter a String: ");
-            boolean b = onePointOne(scan.nextLine());
-            System.out.println(b);
-        }
-    } 
-    
     public String onePointTwo(String inString) {
         // Write code to reverse a C-Style String  (C-String means that “abcd” is 
         // represented as five characters, including the null character )
@@ -46,18 +37,67 @@ public class Chapter1 {
         return String.valueOf(chars);
     }
     
-    public void testOnePointTwo() {
+    public String onePointThree(char[] input) {
+        // Design an algorithm and write code to remove the duplicate characters 
+        // in a string without using any additional buffer  NOTE: One or two 
+        // additional variables are fine. An extra copy of the array is not 
+        int len = input.length;
+        int tail = 1; 
+        if (len >= 2) { //char arrays of length 0 or 1 will not have duplicate chars
+            for (int i = 0; i < len; i++) {
+                int j = 0;
+                while (j < tail && !(input[i]==input[j])) {
+                    j++;
+                }
+                if (j == tail) {
+                    input[tail] = input[i];
+                    tail++;
+                }
+            }
+            while (tail < len) { //clear all chars in array with index >= tail
+                input[tail] = 0;
+                tail++;
+            }
+        }
+        return String.valueOf(input);
+    }
+    
+    public void test() {
         scan  = new Scanner(System.in);
-        for (int i = 0; i < 5; i++ ){
-            System.out.print("Enter a String: ");
-            String s = onePointTwo(scan.nextLine());
-            System.out.println(s);
+        
+        System.out.print("Enter a problem number: ");
+        int problemNum = Integer.valueOf(scan.nextLine());
+        System.out.print("Enter number of repetitions: ");
+        int repetitions = Integer.valueOf(scan.nextLine());
+        System.out.println("");
+        
+        switch(problemNum) {
+            case 1 : // 1.1 determine if string has all unique characters
+                for (int i = 0; i < repetitions; i++ ){
+                    System.out.print("Enter a String to determine whether characters are unique: ");
+                    System.out.println(String.valueOf(onePointOne(scan.nextLine())));
+                }
+                break;      
+            case 2 : // 1.2 reverses a string
+                for (int i = 0; i < repetitions; i++ ){
+                    System.out.print("Enter a String to reverse: ");
+                    System.out.println(onePointTwo(scan.nextLine()));
+                }
+                break;
+            case 3 : // 1.3 removes duplicate characters from a string
+                for (int i = 0; i < repetitions; i++ ){
+                    System.out.print("Enter a String to remove duplicates: ");
+                    System.out.println(onePointThree(scan.nextLine().toCharArray()));
+                }
+                break;
+            default:
+                System.out.println("Invalid problem number.");
+                break;         
         }
     } 
-    
+
     public static void main(String[] args) {
         Chapter1 c1 = new Chapter1();
-        //c1.testOnePointOne();
-        c1.testOnePointTwo();
+        c1.test();
     }
 }
